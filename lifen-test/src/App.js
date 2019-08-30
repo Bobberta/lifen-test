c16a22f7cb63101b2c1e0a346daa6907b5fc0291
Bobberta <anais.limpalaer@lifen.fr>
2019-08-30 22:37:19 +0200

    lvl-1

--- lifen-test/src/App.js
+++ lifen-test/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    files: [],
+    latestFileName: undefined,
+    error: ''
+  };
+  handleOnDrop = (file) => {
+
+    if (file.length === 1) {
+      this.setState((prevState) => ({ 
+        files: prevState.files.concat(file),
+        latestFileName: file[file.length - 1].name,
+        error: ''
+      }));
+    } else {
+      this.setState(() => ({ 
+        latestFileName: '',
+        error: 'Vous ne pouvez sélectionner qu\'un seul document à la fois'
+      }));
+    };
+  };
+  render() {
+      return (
+      <div className="App">
+        <h1><strong>Lifen Document Uploader</strong></h1>
+        <Dropzone onDrop={e => this.handleOnDrop(e)} multiple={false}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+              <div {...getRootProps()} className="dragdrop">
+                <input {...getInputProps()} />
+                <p>Faire glisser un document médical</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <div>
+          {this.state.latestFileName && <p class="success"><strong>{this.state.latestFileName}</strong> a été téléchargé.</p>}
+          {this.state.error && <p class="error">{this.state.error}</p>}
+        </div>
+      </div>   
+    ) 
+  }
+};
 
 export default App;
